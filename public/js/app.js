0ced6eef4e8719cb7ec35d3c137a7a2ac3cb0a61
José Carlos <carlos.avellar@gmail.com>
2018-09-14 17:20:35 -0300

    Ok

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,16 @@
 let countDate = new Date("March, 02, 2019, 19:00:00").getTime();
 let numDias, numHoras, numMin, numSeg, distance;
-const formtDigits = (val) =>{  
-   if(val < 10){
-    val = "0" + val;
+const formtDigits = (val) => {
+    if (val < 10) {
+        val = "0" + val;
     }
     return val;
 };
-const updateClock = ()=>{
+const updateClock = () => {
     let now = new Date().getTime();
     distance = countDate - now;
     // let numMes = new Date().getMonth();
-    console.log( numMes - 6 );
+    console.log(numMes - 6);
     let numMes = Math.floor();
     let numDias = Math.floor(distance / (1000 * 60 * 60 * 24));
     let numHoras = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -20,35 +20,45 @@ const updateClock = ()=>{
     document.querySelector('.dias').innerHTML = formtDigits(numDias);
     document.querySelector('.horas').innerHTML = formtDigits(numHoras);
     document.querySelector('.minutos').innerHTML = formtDigits(numMin);
-    document.querySelector('.segundos').innerHTML = formtDigits(numSeg);  
+    document.querySelector('.segundos').innerHTML = formtDigits(numSeg);
 };
 setInterval(updateClock, 1000);
 updateClock();
-if(distance < 0){
-    clearInterval(updateClock) 
+if (distance < 0) {
+    clearInterval(updateClock)
     console.log('___setInterval cleared-');
 }
 
-
-//// VIDEO 
-const btnPlay =  document.getElementById("playBtn");
+//// Video
+const btnPlay = document.getElementById("playBtn");
 const fullVideo = document.getElementById("myVideo-full");
-const myVideo =  document.getElementById("myVideo");
+const myVideo = document.getElementById("myVideo");
 const btnClose = document.getElementById("closeBot");
-btnPlay.addEventListener('click',()=>{
-     myVideo.pause();
+btnPlay.addEventListener('click', () => {
+    myVideo.pause();
 });
-document.getElementById("closeBot").addEventListener('click', ()=>{
+document.getElementById("closeBot").addEventListener('click', () => {
     myVideo.play();
 
 });
 fullVideo.controls = true;
-
-document.addEventListener("scroll", ()=>{
-        if(window.scrollY > 500){
-            console.log("Maior que 100");
-            myVideo.pause();
-        }else{
-            myVideo.play();
-        }
+document.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+        console.log("Maior que 100");
+        myVideo.pause();
+    } else {
+        myVideo.play();
+    }
 });
+
+console.log('Total love');
+// Load 1
+for(let i=0;i<10;i++){
+    console.log("Do something");
+}
+document.addEventListener("DOMContentLoaded", ()=>{
+    setTimeout(()=>{
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("wrapper").style.visibility= "initial";
+    },110000);
+}, false);
