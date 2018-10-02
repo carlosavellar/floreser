26580ef2129f6aabedae688c47ddfc00159123f3
José Carlos <carlos.avellar@gmail.com>
2018-10-02 08:24:32 -0300

    Beauty code

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
+const btnPlay = document.getElementById("playBtn2");
+const fullVideo = document.getElementById("myVideo-full");
+const myVideo = document.getElementById("myVideo");
+const btnClose = document.getElementById("closeBot");
+const modalVideo = document.getElementById("exampleModalCenter");
 let countDate = new Date("March, 02, 2019, 19:00:00").getTime();
 let numDias, numHoras, numMin, numSeg, distance;
+
 const formtDigits = (val) => {
     if (val < 10) {
         val = "0" + val;
@@ -16,7 +22,6 @@ const updateClock = () => {
     let numHoras = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let numMin = Math.floor((distance % (1000 * 60 * 60)) / ((1000 * 60)));
     let numSeg = Math.floor((distance % (1000 * 60)) / 1000);
-
     document.querySelector('.dias').innerHTML = formtDigits(numDias);
     document.querySelector('.horas').innerHTML = formtDigits(numHoras);
     document.querySelector('.minutos').innerHTML = formtDigits(numMin);
@@ -28,51 +33,30 @@ if (distance < 0) {
     clearInterval(updateClock)
     console.log('___setInterval cleared-');
 }
-
 //// Video
-const btnPlay = document.getElementById("playBtn2");
-const fullVideo = document.getElementById("myVideo-full");
-const myVideo = document.getElementById("myVideo");
-const btnClose = document.getElementById("closeBot");
-const modalVideo = document.getElementById("exampleModalCenter");
+fullVideo.controls = true;
+fullVideo.pause();
+fullVideo.loop = false;
+fullVideo.muted = false;
 
 document.addEventListener('click', el=>{
     if(el.target === modalVideo){
         console.log(`no target ${el}`);
         fullVideo.pause();
+        myVideo.play();
     }else if(!modalVideo === "clicked"){
         console.log('Fora dele');
-    }
-});
-
-btnPlay.addEventListener('click', () => {
-    myVideo.pause();
-});
-document.getElementById("closeBot").addEventListener('click', () => {
-    myVideo.play();
-
-});
-fullVideo.controls = true;
-fullVideo.pause();
-document.addEventListener("scroll", () => {
-    if (window.scrollY > 500) {
-        // console.log("Maior que 100");
-        myVideo.pause();
-    } else {
         myVideo.play();
     }
 });
-fullVideo.loop = false;
-fullVideo.muted = false;
-closeBot.addEventListener('click', ()=>{ 
-    fullVideo.pause();
+btnPlay.addEventListener('click', () => {
+    myVideo.pause();
+    fullVideo.play();
 });
-
-if(!modalVideo.classList.contains(".show")){
-    // console.log('Show false');
-    fullVideo.pause();
-}else{
-    
-    // console.log('Show true');
-}
+document.getElementById("closeBot").addEventListener('click', () => myVideo.play());
+document.addEventListener("scroll", () => {
+    window.scrollY > 500 ?  myVideo.pause() : myVideo.play();
+});
+closeBot.addEventListener('click', ()=> fullVideo.pause());
+!modalVideo.classList.contains(".show") ? fullVideo.pause() : console.log("Show true");
 
